4ed6e4d47d54955d3732ecab72925192641a825e
Fzhou1997 <fzhou1997@gmail.com>
2024-11-01 14:57:42 -0400

    implemented inner activation layers in multi layer modules. working on multi layer lstm with layer norm or recurrent batch norm

--- utils_torch/modules/MultiLayerConv1d.py
+++ utils_torch/modules/MultiLayerConv1d.py
@@ -1,11 +1,10 @@
-from typing import Sequence, Literal
+from typing import Sequence, Literal, Type
 
 from torch import nn, Tensor
 from torch.nn.common_types import _size_1_t
 
 
-_Activation = Literal[
-    "none",
+_activations = [
     "Threshold",
     "ReLU",
     "RReLU",
@@ -43,6 +42,8 @@ class MultiLayerConv1d(nn.Module):
 
     Attributes:
         num_conv_layers (int): Number of convolutional layers.
+        num_inner_layers (int): Number of inner layers.
+
         conv_channel_size (Sequence[int]): Number of channels in each convolutional layer.
         conv_kernel_size (Sequence[int]): Kernel size for each convolutional layer.
         conv_stride (Sequence[int]): Stride for each convolutional layer.
@@ -51,15 +52,20 @@ class MultiLayerConv1d(nn.Module):
         conv_groups (Sequence[int]): Number of groups for each convolutional layer.
         conv_bias (Sequence[bool]): Whether to use bias in each convolutional layer.
         conv_padding_mode (Sequence[Literal['zeros', 'reflect', 'replicate', 'circular']]): Padding mode for each convolutional layer.
-        num_inner_layers (int): Number of inner layers.
+
+        activation (Sequence[Type[nn.Module]]): Activation function for each layer.
+        activation_kwargs (Sequence[dict[str, any]]): Keyword arguments for the activation function.
+
+        dropout_p (Sequence[float]): Dropout probability for each dropout layer.
+        dropout_inplace (Sequence[bool]): Whether to perform dropout in-place.
+        dropout_first (Sequence[bool]): Whether to perform dropout before the batch normalization layer.
+
         batch_norm (Sequence[bool]): Whether to apply batch normalization.
         batch_norm_eps (Sequence[float]): Epsilon value for batch normalization.
         batch_norm_momentum (Sequence[float]): Momentum value for batch normalization.
         batch_norm_affine (Sequence[bool]): Whether to learn affine parameters in batch normalization.
         batch_norm_track_running_stats (Sequence[bool]): Whether to track running statistics in batch normalization.
-        dropout_p (Sequence[float]): Dropout probability for each dropout layer.
-        dropout_inplace (Sequence[bool]): Whether to perform dropout in-place.
-        dropout_first (Sequence[bool]): Whether to perform dropout before the convolutional layer.
+
         modules (nn.Sequential): Sequential container of layers.
     """
 
@@ -75,15 +81,18 @@ class MultiLayerConv1d(nn.Module):
     conv_bias: Sequence[bool]
     conv_padding_mode: Sequence[Literal['zeros', 'reflect', 'replicate', 'circular']]
 
+    activation: Sequence[Type[nn.Module]]
+    activation_kwargs: Sequence[dict[str, any]]
+
+    dropout_p: Sequence[float]
+    dropout_inplace: Sequence[bool]
+    dropout_first: Sequence[bool]
+
     batch_norm: Sequence[bool]
     batch_norm_eps: Sequence[float]
     batch_norm_momentum: Sequence[float]
     batch_norm_affine: Sequence[bool]
     batch_norm_track_running_stats: Sequence[bool]
-
-    dropout_p: Sequence[float]
-    dropout_inplace: Sequence[bool]
-    dropout_first: Sequence[bool]
 
     modules: nn.Sequential
 
@@ -97,14 +106,16 @@ class MultiLayerConv1d(nn.Module):
                  groups: int | Sequence[int] = 1,
                  bias: bool | Sequence[bool] = True,
                  padding_mode: Literal['zeros', 'reflect', 'replicate', 'circular'] | Sequence[Literal['zeros', 'reflect', 'replicate', 'circular']] = 'zeros',
+                 activation: Type[nn.Module] | Sequence[Type[nn.Module]] = nn.ReLU,
+                 activation_kwargs: dict[str, any] | Sequence[dict[str, any]] = None,
+                 dropout_p: float | Sequence[float] = 0.5,
+                 dropout_inplace: bool | Sequence[bool] = True,
+                 dropout_first: bool | Sequence[bool] = False,
                  batch_norm: bool | Sequence[bool] = True,
                  batch_norm_eps: float | Sequence[float] = 1e-5,
                  batch_norm_momentum: float | Sequence[float] = 0.1,
                  batch_norm_affine: bool | Sequence[bool] = True,
-                 batch_norm_track_running_stats: bool | Sequence[bool] = True,
-                 dropout_p: float | Sequence[float] = 0.5,
-                 dropout_inplace: bool | Sequence[bool] = False,
-                 dropout_first: bool | Sequence[bool] = False) -> None:
+                 batch_norm_track_running_stats: bool | Sequence[bool] = True) -> None:
         """
         Initializes the ModuleMultiLayerConv1d.
 
@@ -118,21 +129,25 @@ class MultiLayerConv1d(nn.Module):
             groups (int | Sequence[int], optional): Number of groups for each layer, default is 1.
             bias (bool | Sequence[bool], optional): Whether to use bias in each layer, default is True.
             padding_mode (Literal['zeros', 'reflect', 'replicate', 'circular'] | Sequence[Literal['zeros', 'reflect', 'replicate', 'circular']], optional): Padding mode for each layer, default is 'zeros'.
+            activation (Type[nn.Module] | Sequence[Type[nn.Module]], optional): Activation function for each layer, default is nn.ReLU.
+            activation_kwargs (dict[str, Any] | Sequence[dict[str, Any]], optional): Keyword arguments for the activation function, default is None.
+            dropout_p (float | Sequence[float], optional): Dropout probability for each dropout layer, default is 0.5.
+            dropout_inplace (bool | Sequence[bool], optional): Whether to perform dropout in-place, default is False.
+            dropout_first (bool | Sequence[bool], optional): Whether to perform dropout before the batch normalization layer, default is True.
             batch_norm (bool | Sequence[bool], optional): Whether to apply batch normalization, default is True.
             batch_norm_eps (float | Sequence[float], optional): Epsilon value for batch normalization, default is 1e-5.
             batch_norm_momentum (float | Sequence[float], optional): Momentum value for batch normalization, default is 0.1.
             batch_norm_affine (bool | Sequence[bool], optional): Whether to learn affine parameters in batch normalization, default is True.
             batch_norm_track_running_stats (bool | Sequence[bool], optional): Whether to track running statistics in batch normalization, default is True.
-            dropout_p (float | Sequence[float], optional): Dropout probability for each dropout layer, default is 0.5.
-            dropout_inplace (bool | Sequence[bool], optional): Whether to perform dropout in-place, default is False.
-            dropout_first (bool | Sequence[bool], optional): Whether to perform dropout before the convolutional layer, default is False.
 
         Raises:
             AssertionError: If num_layers is less than or equal to 0.
-            AssertionError: If batch_norm, batch_norm_eps, batch_norm_momentum, batch_norm_affine, and batch_norm_track_running_stats do not have length num_layers.
             AssertionError: If channel_size does not have length num_layers + 1.
             AssertionError: If kernel_size, stride, padding, dilation, groups, bias, and padding_mode do not have length num_layers.
-            AssertionError: If dropout_p, dropout_inplace, dropout_first does not have length num_layers - 1.
+            AssertionError: If activation, activation_kwargs do not have length num_layers - 1.
+            AssertionError: If activation is not one of the supported activation functions.
+            AssertionError: If dropout_p, dropout_inplace, dropout_first do not have length num_layers - 1.
+            AssertionError: If batch_norm, batch_norm_eps, batch_norm_momentum, batch_norm_affine, and batch_norm_track_running_stats do not have length num_layers - 1.
         """
 
         super(MultiLayerConv1d, self).__init__()
@@ -159,6 +174,20 @@ class MultiLayerConv1d(nn.Module):
         if isinstance(padding_mode, str):
             padding_mode = [padding_mode] * self.num_conv_layers
 
+        if isinstance(activation, type):
+            activation = [activation] * self.num_inner_layers
+        if isinstance(activation_kwargs, dict):
+            activation_kwargs = [activation_kwargs] * self.num_inner_layers
+        elif activation_kwargs is None:
+            activation_kwargs = [{}] * self.num_inner_layers
+
+        if isinstance(dropout_p, float):
+            dropout_p = [dropout_p] * self.num_inner_layers
+        if isinstance(dropout_inplace, bool):
+            dropout_inplace = [dropout_inplace] * self.num_inner_layers
+        if isinstance(dropout_first, bool):
+            dropout_first = [dropout_first] * self.num_inner_layers
+
         if isinstance(batch_norm, bool):
             batch_norm = [batch_norm] * self.num_inner_layers
         if isinstance(batch_norm_eps, float):
@@ -170,13 +199,6 @@ class MultiLayerConv1d(nn.Module):
         if isinstance(batch_norm_track_running_stats, bool):
             batch_norm_track_running_stats = [batch_norm_track_running_stats] * self.num_inner_layers
 
-        if isinstance(dropout_p, float):
-            dropout_p = [dropout_p] * self.num_inner_layers
-        if isinstance(dropout_inplace, bool):
-            dropout_inplace = [dropout_inplace] * self.num_inner_layers
-        if isinstance(dropout_first, bool):
-            dropout_first = [dropout_first] * self.num_inner_layers
-
         assert len(channel_size) == num_layers + 1, 'channel_size must have length num_layers + 1'
         assert len(kernel_size) == num_layers, 'kernel_size must have length num_layers'
         assert len(stride) == num_layers, 'stride must have length num_layers'
@@ -186,15 +208,19 @@ class MultiLayerConv1d(nn.Module):
         assert len(bias) == num_layers, 'bias must have length num_layers'
         assert len(padding_mode) == num_layers, 'padding_mode must have length num_layers'
 
+        assert len(activation) == num_layers - 1, 'activation must have length num_layers - 1'
+        assert len(activation_kwargs) == num_layers - 1, 'activation_kwargs must have length num_layers - 1'
+        assert all([cls is None or cls.__name__ in _activations for cls in activation]), 'activation must be one of the following types: ' + ', '.join(_activations)
+
+        assert len(dropout_p) == num_layers - 1, 'dropout_p must have length num_layers - 1'
+        assert len(dropout_inplace) == num_layers - 1, 'dropout_inplace must have length num_layers - 1'
+        assert len(dropout_first) == num_layers - 1, 'dropout_first must have length num_layers - 1'
+
         assert len(batch_norm) == num_layers - 1, 'batch_norm must have length num_layers - 1'
         assert len(batch_norm_eps) == num_layers - 1, 'batch_norm_eps must have length num_layers - 1'
         assert len(batch_norm_momentum) == num_layers - 1, 'batch_norm_momentum must have length num_layers - 1'
         assert len(batch_norm_affine) == num_layers - 1, 'batch_norm_affine must have length num_layers - 1'
         assert len(batch_norm_track_running_stats) == num_layers - 1, 'batch_norm_track_running_stats must have length num_layers - 1'
-
-        assert len(dropout_p) == num_layers - 1, 'dropout_p must have length num_layers - 1'
-        assert len(dropout_inplace) == num_layers - 1, 'dropout_inplace must have length num_layers - 1'
-        assert len(dropout_first) == num_layers - 1, 'dropout_first must have length num_layers - 1'
 
         self.conv_channel_size = channel_size
         self.conv_kernel_size = kernel_size
@@ -205,15 +231,18 @@ class MultiLayerConv1d(nn.Module):
         self.conv_bias = bias
         self.conv_padding_mode = padding_mode
 
+        self.activation = activation
+        self.activation_kwargs = activation_kwargs
+
+        self.dropout_p = dropout_p
+        self.dropout_inplace = dropout_inplace
+        self.dropout_first = dropout_first
+
         self.batch_norm = batch_norm
         self.batch_norm_eps = batch_norm_eps
         self.batch_norm_momentum = batch_norm_momentum
         self.batch_norm_affine = batch_norm_affine
         self.batch_norm_track_running_stats = batch_norm_track_running_stats
-
-        self.dropout_p = dropout_p
-        self.dropout_inplace = dropout_inplace
-        self.dropout_first = dropout_first
 
         self.modules = nn.Sequential()
 
@@ -228,6 +257,7 @@ class MultiLayerConv1d(nn.Module):
                                           bias=bias[i]))
             if i >= self.num_inner_layers:
                 continue
+            self.modules.append(activation[i](**activation_kwargs[i]))
             if dropout_first[i]:
                 if dropout_p[i] > 0:
                     self.modules.append(nn.Dropout(p=dropout_p[i],
--- utils_torch/modules/MultiLayerLinear.py
+++ utils_torch/modules/MultiLayerLinear.py
@@ -1,7 +1,39 @@
-from typing import Sequence
+from typing import Sequence, Type
 
 from torch import nn
 
+
+_activations = [
+    "Threshold",
+    "ReLU",
+    "RReLU",
+    "Hardtanh",
+    "ReLU6",
+    "Sigmoid",
+    "Hardsigmoid",
+    "Tanh",
+    "SiLU",
+    "Mish",
+    "Hardswish",
+    "ELU",
+    "CELU",
+    "SELU",
+    "GLU",
+    "GELU",
+    "Hardshrink",
+    "LeakyReLU",
+    "LogSigmoid",
+    "Softplus",
+    "Softshrink",
+    "MultiheadAttention",
+    "PReLU",
+    "Softsign",
+    "Tanhshrink",
+    "Softmin",
+    "Softmax",
+    "Softmax2d",
+    "LogSoftmax",
+]
 
 class MultiLayerLinear(nn.Module):
     """
@@ -14,17 +46,20 @@ class MultiLayerLinear(nn.Module):
         linear_num_features (Sequence[int]): A list of the number of features in each layer.
         linear_bias (Sequence[bool]): A list of booleans indicating whether to add a learnable bias to the output.
 
-        batch_norm (Sequence[bool]): A list of booleans indicating whether to use batch normalization for each layer.
-        batch_norm_eps (Sequence[float]): A list of epsilon values added to the denominator for numerical stability.
-        batch_norm_momentum (Sequence[float]): A list of values used for the running_mean and running_var computation.
-        batch_norm_affine (Sequence[bool]): A list of booleans indicating whether the module has learnable affine parameters.
-        batch_norm_track_running_stats (Sequence[bool]): A list of booleans indicating whether the module tracks the running mean and variance.
+        activation (Sequence[Type[nn.Module]]): Activation function for each layer.
+        activation_kwargs (Sequence[dict[str, any]]): Keyword arguments for the activation function.
 
-        dropout_p (Sequence[float]): A list of dropout probabilities for each layer.
-        dropout_inplace (Sequence[bool]): A list of booleans indicating whether to operate in-place.
-        dropout_first (Sequence[bool]): If True, applies dropout before the batch normalization layer.
+        dropout_p (Sequence[float]): Dropout probability for each dropout layer.
+        dropout_inplace (Sequence[bool]): Whether to perform dropout in-place.
+        dropout_first (Sequence[bool]): Whether to perform dropout before the batch normalization layer.
 
-        modules (nn.Sequential): The sequential container for the module.
+        batch_norm (Sequence[bool]): Whether to apply batch normalization.
+        batch_norm_eps (Sequence[float]): Epsilon value for batch normalization.
+        batch_norm_momentum (Sequence[float]): Momentum value for batch normalization.
+        batch_norm_affine (Sequence[bool]): Whether to learn affine parameters in batch normalization.
+        batch_norm_track_running_stats (Sequence[bool]): Whether to track running statistics in batch normalization.
+
+        modules (nn.Sequential): Sequential container of layers.
     """
 
     num_linear_layers: int
@@ -33,51 +68,61 @@ class MultiLayerLinear(nn.Module):
     linear_num_features: Sequence[int]
     linear_bias: Sequence[bool]
 
+    activation: Sequence[Type[nn.Module]]
+    activation_kwargs: Sequence[dict[str, any]]
+
+    dropout_p: Sequence[float]
+    dropout_inplace: Sequence[bool]
+    dropout_first: Sequence[bool]
+
     batch_norm: Sequence[bool]
     batch_norm_eps: Sequence[float]
     batch_norm_momentum: Sequence[float]
     batch_norm_affine: Sequence[bool]
     batch_norm_track_running_stats: Sequence[bool]
 
-    dropout_p: Sequence[float]
-    dropout_inplace: Sequence[bool]
-    dropout_first: Sequence[bool]
-
-    modules = nn.Sequential
+    modules: nn.Sequential
 
     def __init__(self,
                  num_layers: int,
                  num_features: int | Sequence[int],
-                 bias: bool | Sequence[bool],
-                 batch_norm: bool | Sequence[bool],
-                 batch_norm_eps: float | Sequence[float],
-                 batch_norm_momentum: float | Sequence[float],
-                 batch_norm_affine: bool | Sequence[bool],
-                 batch_norm_track_running_stats: bool | Sequence[bool],
-                 dropout_p: float | Sequence[float],
-                 dropout_inplace: bool | Sequence[bool],
-                 dropout_first: bool | Sequence[bool]):
+                 bias: bool | Sequence[bool] = True,
+                 activation: Type[nn.Module] | Sequence[Type[nn.Module]] = nn.ReLU,
+                 activation_kwargs: dict[str, any] | Sequence[dict[str, any]] = None,
+                 dropout_p: float | Sequence[float] = 0.5,
+                 dropout_inplace: bool | Sequence[bool] = True,
+                 dropout_first: bool | Sequence[bool] = False,
+                 batch_norm: bool | Sequence[bool] = True,
+                 batch_norm_eps: float | Sequence[float] = 1e-5,
+                 batch_norm_momentum: float | Sequence[float] = 0.1,
+                 batch_norm_affine: bool | Sequence[bool] = True,
+                 batch_norm_track_running_stats: bool | Sequence[bool] = True) -> None:
         """
         Initializes the multi-layer linear module.
 
         Args:
             num_layers (int): The number of linear layers in the module.
             num_features (int | Sequence[int]): The number of features in each layer.
-            bias (bool | Sequence[bool]): If True, adds a learnable bias to the output.
-            dropout_p (float | Sequence[float]): If non-zero, introduces a Dropout layer on the outputs of each linear layer except the last layer.
-            dropout_inplace (bool | Sequence[bool]): If True, operates in-place.
-            batch_norm (bool | Sequence[bool]): If True, adds a BatchNorm1d layer before each linear layer.
-            batch_norm_eps (float | Sequence[float]): A value added to the denominator for numerical stability.
-            batch_norm_momentum (float | Sequence[float]): The value used for the running_mean and running_var computation.
-            batch_norm_affine (bool | Sequence[bool]): If True, this module has learnable affine parameters.
-            batch_norm_track_running_stats (bool | Sequence[bool]): If True, this module tracks the running mean and variance.
+            bias (bool | Sequence[bool], optional): Whether to use bias in each layer, default is True.
+            activation (Type[nn.Module] | Sequence[Type[nn.Module]], optional): Activation function for each layer, default is nn.ReLU.
+            activation_kwargs (dict[str, any] | Sequence[dict[str, any]], optional): Keyword arguments for the activation function, default is None.
+            dropout_p (float | Sequence[float], optional): Dropout probability for each dropout layer, default is 0.5.
+            dropout_inplace (bool | Sequence[bool], optional): Whether to perform dropout in-place, default is False.
+            dropout_first (bool | Sequence[bool], optional): Whether to perform dropout before the batch normalization layer, default is True.
+            batch_norm (bool | Sequence[bool], optional): Whether to apply batch normalization, default is True.
+            batch_norm_eps (float | Sequence[float], optional): Epsilon value for batch normalization, default is 1e-5.
+            batch_norm_momentum (float | Sequence[float], optional): Momentum value for batch normalization, default is 0.1.
+            batch_norm_affine (bool | Sequence[bool], optional): Whether to learn affine parameters in batch normalization, default is True.
+            batch_norm_track_running_stats (bool | Sequence[bool], optional): Whether to track running statistics in batch normalization, default is True.
 
         Raises:
             AssertionError: If num_layers is less than or equal to 0.
-            AssertionError: If batch_norm, batch_norm_eps, batch_norm_momentum, batch_norm_affine, and batch_norm_track_running_stats do not have length num_layers.
             AssertionError: If num_features does not have length num_layers + 1.
             AssertionError: If bias does not have length num_layers.
-            AssertionError: If dropout_p, dropout_inplace does not have length num_layers - 1.
+            AssertionError: If activation, activation_kwargs do not have length num_layers - 1.
+            AssertionError: If activation is not one of the supported activation functions.
+            AssertionError: If dropout_p, dropout_inplace do not have length num_layers - 1.
+            AssertionError: If batch_norm, batch_norm_eps, batch_norm_momentum, batch_norm_affine, and batch_norm_track_running_stats do not have length num_layers - 1.
         """
         super(MultiLayerLinear, self).__init__()
 
@@ -91,6 +136,20 @@ class MultiLayerLinear(nn.Module):
         if isinstance(bias, bool):
             bias = [bias] * self.num_linear_layers
 
+        if isinstance(activation, type):
+            activation = [activation] * self.num_inner_layers
+        if isinstance(activation_kwargs, dict):
+            activation_kwargs = [activation_kwargs] * self.num_inner_layers
+        if activation_kwargs is None:
+            activation_kwargs = [{}] * self.num_inner_layers
+
+        if isinstance(dropout_p, float):
+            dropout_p = [dropout_p] * self.num_inner_layers
+        if isinstance(dropout_inplace, bool):
+            dropout_inplace = [dropout_inplace] * self.num_inner_layers
+        if isinstance(dropout_first, bool):
+            dropout_first = [dropout_first] * self.num_inner_layers
+
         if isinstance(batch_norm, bool):
             batch_norm = [batch_norm] * self.num_inner_layers
         if isinstance(batch_norm_eps, float):
@@ -102,15 +161,16 @@ class MultiLayerLinear(nn.Module):
         if isinstance(batch_norm_track_running_stats, bool):
             batch_norm_track_running_stats = [batch_norm_track_running_stats] * self.num_inner_layers
 
-        if isinstance(dropout_p, float):
-            dropout_p = [dropout_p] * self.num_inner_layers
-        if isinstance(dropout_inplace, bool):
-            dropout_inplace = [dropout_inplace] * self.num_inner_layers
-        if isinstance(dropout_first, bool):
-            dropout_first = [dropout_first] * self.num_inner_layers
-
         assert len(num_features) == num_layers + 1, 'linear_num_features must have length num_layers + 1'
         assert len(bias) == num_layers, 'linear_bias must have length num_layers'
+
+        assert len(activation) == num_layers - 1, 'activation must have length num_layers - 1'
+        assert len(activation_kwargs) == num_layers - 1, 'activation_kwargs must have length num_layers - 1'
+        assert all([cls is None or cls.__name__ in _activations for cls in activation]), 'activation must be one of the following types: ' + ', '.join(_activations)
+
+        assert len(dropout_p) == num_layers - 1, 'dropout_p must have length num_layers - 1'
+        assert len(dropout_inplace) == num_layers - 1, 'dropout_inplace must have length num_layers - 1'
+        assert len(dropout_first) == num_layers - 1, 'dropout_first must have length num_layers - 1'
 
         assert len(batch_norm) == num_layers - 1, 'batch_norm must have length num_layers - 1'
         assert len(batch_norm_eps) == num_layers - 1, 'batch_norm_eps must have length num_layers - 1'
@@ -118,22 +178,21 @@ class MultiLayerLinear(nn.Module):
         assert len(batch_norm_affine) == num_layers - 1, 'batch_norm_affine must have length num_layers - 1'
         assert len(batch_norm_track_running_stats) == num_layers - 1, 'batch_norm_track_running_stats must have length num_layers - 1'
 
-        assert len(dropout_p) == num_layers - 1, 'dropout_p must have length num_layers - 1'
-        assert len(dropout_inplace) == num_layers - 1, 'dropout_inplace must have length num_layers - 1'
-        assert len(dropout_first) == num_layers - 1, 'dropout_first must have length num_layers - 1'
-
         self.linear_num_features = num_features
         self.linear_bias = bias
+
+        self.activation = activation
+        self.activation_kwargs = activation_kwargs
+
+        self.dropout_p = dropout_p
+        self.dropout_inplace = dropout_inplace
+        self.dropout_first = dropout_first
 
         self.batch_norm = batch_norm
         self.batch_norm_eps = batch_norm_eps
         self.batch_norm_momentum = batch_norm_momentum
         self.batch_norm_affine = batch_norm_affine
         self.batch_norm_track_running_stats = batch_norm_track_running_stats
-
-        self.dropout_p = dropout_p
-        self.dropout_inplace = dropout_inplace
-        self.dropout_first = dropout_first
 
         self.modules = nn.Sequential()
 
@@ -143,6 +202,7 @@ class MultiLayerLinear(nn.Module):
                                           bias=bias[i]))
             if i >= self.num_inner_layers:
                 continue
+            self.modules.append(activation[i](**activation_kwargs[i]))
             if dropout_first[i]:
                 if dropout_p[i] > 0:
                     self.modules.append(nn.Dropout(p=dropout_p[i],
