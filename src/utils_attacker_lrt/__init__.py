d9821cc4474a5074d53257bb6137527d5ed15843
Fzhou1997 <fzhou1997@gmail.com>
2024-12-30 13:24:09 -0500

    Simplify imports in `utils_attacker_lrt` module.
    Removed an unused import and adjusted the import structure for clarity and maintainability. This ensures that only relevant components are included, reducing unnecessary dependencies.

--- src/utils_attacker_lrt/__init__.py
+++ src/utils_attacker_lrt/__init__.py
@@ -1,4 +1,4 @@
+from .likelihood_ratio_statistic_aaf import *
 from .likelihood_ratio_statistic_beacon import *
-from .likelihood_ratio_statistic_pool import *
 from .likelihood_ratio_test import *
 from .site_frequency_spectrum import *
